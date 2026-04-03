5907402d414bdc21c3cd67ee0ac728732b8dd900
chungchihhan <harryup2000@gmail.com>
2026-04-03 13:28:19 +0800

    chore: init repo

---
+++ JotKit/JotKitApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+struct JotKitApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+    var body: some Scene {
+        // All window management is in AppDelegate.
+        // Settings scene prevents SwiftUI from quitting when last window closes.
+        Settings { EmptyView() }
+    }
+}
